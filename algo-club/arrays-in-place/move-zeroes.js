1da0cf36a17f0c82a92925b2222b998f7f6b9b23
Briseida Pagador <bpagador@gmail.com>
2020-11-10 15:08:03 -0800

    solution to move-zeroes

--- algo-club/arrays-in-place/move-zeroes.js
+++ algo-club/arrays-in-place/move-zeroes.js
@@ -7,3 +7,28 @@ Input: [0,1,0,3,12]
 Output: [1,3,12,0,0]
 */
 
+const moveZeroes = (input) => {
+  // move zeroes to the end of the array
+    // iterate through input array 
+    for(let i = 0; i < input.length; i++ ) {
+       // condition that evaluates if number is a zero
+       if(input[i] === 0) {
+        // push zeroes to end of the array
+        input.push(input[i]);
+        // delete zero from the og spot it was in 
+        input.splice(i, 1);
+        
+       } 
+    }
+    console.log(input)
+    return input;
+    
+   
+     
+    
+
+  // keep the other numbers in the same order
+    // this would naturally happen by moving zeroes to the end 
+}
+
+  
