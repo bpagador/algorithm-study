52079db57501f01a828db52c6cda477f61e9d1b7
Briseida Pagador <bpagador@gmail.com>
2020-11-19 11:38:58 -0800

    fail safe commit

--- algo-club/hash-tables/repeated-N-size-2N.js
+++ algo-club/hash-tables/repeated-N-size-2N.js
@@ -1,1 +1,23 @@
+// we have a list of int that contains at least one repeating int
+// we want to find the repeating int
 
+
+
+const repeatedNTimes = (A) => {
+  //create a dictionary 
+    // const seenIntegers = {}
+      let integersSeen = {};
+      
+  // iterate through the list of integers
+    // for (A.length)
+      for (let i = 0; i < A.length; i++) {
+          
+          // if int is not in the dictionary, add to dictionary and assign value
+          if(A[i] in integersSeen) return A[i]
+          
+      // this means its only seen once        
+          integersSeen[A[i]] = 0
+    
+      }
+  
+  }
